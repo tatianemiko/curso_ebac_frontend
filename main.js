9fef76588c1274cfb33e0bce55b8f2d1bb7fb718
Tatiane Keiko <tatiane.kunamitsu@gmail.com>
2023-10-09 16:32:59 -0300

    Correção do exercício

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 $(document).ready(function(){
-    const notaRealizada = $('.nota-clicar');
-    const notaRiscada = $('<button type="submit" class="nota-clicar" style="text-decoration: line-through;"></button>');//===PARA O TUTOR== 
+    const notaRiscado = $('.riscado');
 
     $('form').on('submit', function(e){
         e.preventDefault();
         
         const inputNomeTarefa = $('#nome-tarefa').val();
-        const novaTarefa = $('<li> </li>');
+        const novaTarefa = $('<li>' +inputNomeTarefa+ '</li>');
         
-        $(inputNomeTarefa).appendTo(novaTarefa);//===PARA O TUTOR== aqui eu coloquei assim, mas também não esta funcionando, o valores inserido no input não está indo para a novaTrefa
+        $(novaTarefa).appendTo('ul'); 
+        $('#nome-tarefa').val('');
     })
-    
-    //===PARA O TUTOR== aqui eu pensei em quando clicarem na lista o estilo ser aplicado como está na linha 3, a função de clicar esta rodando, porém o estilo não, então acho que deve estar errado a parte do estilo no "notaRiscada".. como seria uma boa maneira de aplica-lo? Minha linha de pensamento tá muito errada? sempre fico na dúvida kkkkk
-    $(notaRealizada).click(function(){
-            alert("oi")
-            $(notaRiscada);
-        }) 
+
+    $('body').on('click', 'li', function(){
+        $(this).toggleClass('riscado');
+    });
 });
