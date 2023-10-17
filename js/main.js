5d4dae4f03c57f3c2243c314fc20e79dab360acf
Tatiane Keiko <tatiane.kunamitsu@gmail.com>
2023-10-17 11:26:47 -0300

    exercicio jQuery plugins Módulo 10 EBAC

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#telefone').mask('(00)0000-0000',{
+        placeholder: '(00)0000-0000'
+    });
+    $('#cpf').mask('000.000.000-00',{
+        placeholder: '000.000.000-00'
+    });
+    $('#cep').mask('00000-000' ,{
+        placeholder:'00000-000'
+    });
+})
